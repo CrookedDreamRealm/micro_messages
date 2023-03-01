f40547b7df2c3d95068e4e4af3e45595b35eeb8f
Pandacy <anuwat@live.nl>
2023-03-01 10:30:24 +0100

    base micro added

---
+++ src/main/java/com/dreamrealm/micro_messages/MicroMessagesApplication.java
@@ -0,0 +1,13 @@
+package com.dreamrealm.micro_messages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroMessagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamrealm/micro_messages/MicroMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamrealm.micro_messages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroMessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
