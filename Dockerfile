f561e5415dca27554fbc0b7f7af0411693a29cd9
Pandacy <anuwat@live.nl>
2023-04-29 19:52:40 +0200

    changes to dockerfile, added filter, added kubernetes url
    changes to dockerfile, added filter, added kubernetes url

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,5 @@
-FROM eclipse-temurin:17-jdk-jammy as base
-WORKDIR /app
-COPY .mvn/ .mvn
-COPY mvnw pom.xml ./
-RUN ./mvnw dependency:resolve
-COPY src ./src
-
-FROM base as build
-RUN ./mvnw package
-
-FROM eclipse-temurin:17-jre-jammy as production
+FROM eclipse-temurin:17-jre-jammy
 EXPOSE 8080
 #ARG JAR_FILE=app/target/*.jar
-COPY --from=build /app/target/*.jar messages.jar
+COPY /app/target/*.jar messages.jar
 CMD ["java", "-Djava.security.egd=file:/dev/./urandom", "-jar", "/messages.jar"]
--- src/main/java/com/dreamrealm/controller/MessageController.java
+++ src/main/java/com/dreamrealm/controller/MessageController.java
@@ -9,15 +9,14 @@ import com.dreamrealm.repository.MessageRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/message")
 @RestController
 public class MessageController {
    @Autowired
